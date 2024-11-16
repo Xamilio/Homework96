b99318ac9480b195af8c6a054be55c1f57e3289c
Xamilio <119781901+Xamilio@users.noreply.github.com>
2024-11-16 23:32:19 +0300

    Update Homework94.cpp

--- Homework94/Homework94.cpp
+++ Homework94/Homework94.cpp
@@ -1,29 +1,85 @@
 #include <iostream>
-#include <ctime>
+
 using namespace std;
-int main()
+
+int main() 
 {
-    setlocale(LC_ALL, "ru");
-    int x = 0;
-    int y = 0;
-    int c = 0;
-    int quantity = 0;
-    srand(static_cast<unsigned>(time(0)));
-    for (int i = 0; i < 10; i++)
+    const int n = 20;
+    char arr[n];
+    srand(time(0));
+    for (int i = 0; i < n; ++i) 
     {
-        x = rand() % 10;
-        y = rand() % 10;
-        cout << x << " * " << y << " = ";
-        cin >> c;
-        if (x * y == c)
+        arr[i] = 'a' + rand() % 26; 
+    }
+    cout << "Початковий масив: ";
+    for (int i = 0; i < n; ++i) 
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    for (int i = 0; i < n - 1; ++i) 
+    {
+        for (int j = 0; j < n - i - 1; ++j) 
         {
-            cout << "Правильный ответ" << endl;
-            quantity++;
-        }
-        else
-        {
-            cout << "Ответ неверный" << endl;
+            if (arr[j] > arr[j + 1]) {
+                char temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
         }
     }
-    cout << "Вы ответили праильно на " << quantity << "/10";
+    cout << "Масив після сортування (a -> z): ";
+    for (int i = 0; i < n; ++i)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    char target;
+    cout << "Введіть літеру для пошуку: ";
+    cin >> target;
+
+    int left = 0, right = n - 1;
+    bool found = false;
+    while (left <= right) 
+    {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == target)
+        {
+            cout << "Літера '" << target << "' знайдена на індексі " << mid << endl;
+            found = true;
+            break;
+        }
+        else if (arr[mid] < target) 
+        {
+            left = mid + 1;
+        }
+        else 
+        {
+            right = mid - 1;
+        }
+    }
+
+    if (!found)
+    {
+        cout << "Літера '" << target << "' не знайдена." << endl;
+    }
+    for (int i = 0; i < n - 1; ++i)
+    {
+        for (int j = 0; j < n - i - 1; ++j)
+        {
+            if (arr[j] < arr[j + 1])
+            {
+                char temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    cout << "Масив після сортування (z -> a): ";
+    for (int i = 0; i < n; ++i)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 }
